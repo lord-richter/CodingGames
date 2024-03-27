e918d6f77586115f383f2ca932be2e4c31b0101d
Lord Richter <33517321+lord-richter@users.noreply.github.com>
2024-03-27 14:32:32 -0400

    Create solution.py

---
+++ sudoku_validator/solution.py
@@ -0,0 +1,43 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+passed = True
+column = [[ '' for a in range(9)] for b in range(9)]
+cell = [[] for a in range(3)]
+
+for i in range(9):
+    row = input().split()
+    # check row
+    rowsize = len(sorted(set(row)))
+    passed = passed and rowsize==9
+    print(row,rowsize,passed,file=sys.stderr)
+    # save column for later
+    for j in range(9):
+        column[j][i]=row[j]
+    # cells
+    cell[0].extend(row[0:3])
+    cell[1].extend(row[3:6])
+    cell[2].extend(row[6:9])
+    # every third row (9 values) check
+    if len(cell[0])==9 and passed:
+        passed = passed and len(sorted(set(cell[0])))==9
+        passed = passed and len(sorted(set(cell[0])))==9
+        passed = passed and len(sorted(set(cell[0])))==9
+        # new cell
+        cell=[[] for a in range(3)]
+    
+# column check    
+if passed:
+    for i in range(9):
+        colsize = len(sorted(set(column[i])))
+        passed = passed and colsize==9
+
+
+# Write an answer using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+print("true" if passed else "false")
